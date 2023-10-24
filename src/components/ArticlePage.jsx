2fa6314a667910ee6da590f6b01c27910cc08a67
Max Field <maxfield98@outlook.com>
2023-10-24 14:39:20 +0100

    added placeholder comments section to articles

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -9,9 +9,9 @@ export default function ArticlePage() {
   const { article_id } = currentArticle;
 
   useEffect(() => {
+    setIsLoading(true);
     api.fetchArticleById(article_id).then(({ article }) => {
       const [returnedArticle] = article;
-      setIsLoading(true);
       setCurrentArticle(returnedArticle);
       setIsLoading(false);
     });
@@ -22,9 +22,15 @@ export default function ArticlePage() {
   ) : (
     <article>
       <h1>{currentArticle.title}</h1>
+
       <p>Written by {currentArticle.author}</p>
       <img src={currentArticle.article_img_url} alt={currentArticle.title} />
       <p>{currentArticle.body}</p>
+
+      <section>
+        <h3>Comments: {currentArticle.comment_count}</h3>
+        {/* Comments will go here */}
+      </section>
     </article>
   );
 }
