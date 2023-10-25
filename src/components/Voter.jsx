f492bf309f98f17e556e35bbf4845144071bf081
Max Field <maxfield98@outlook.com>
2023-10-25 10:59:51 +0100

    added voting error handling

--- src/components/Voter.jsx
+++ src/components/Voter.jsx
@@ -6,14 +6,23 @@ export default function Voter() {
   const { currentArticle } = useContext(CurrentArticle);
   const [voteDifference, setVoteDifference] = useState(0);
   const [voteCount, setVoteCount] = useState(currentArticle.votes);
+  const [errMsg, setErrMsg] = useState(null);
 
   function updateVotes(value) {
     setVoteDifference((oldState) => oldState + value);
     setVoteCount((oldState) => oldState + value);
 
-    api.incVotes(value, currentArticle.article_id).then((votes) => {
-      setVoteCount(votes);
-    });
+    api
+      .incVotes(value, currentArticle.article_id)
+      .then((votes) => {
+        setVoteCount(votes);
+      })
+      .catch((err) => {
+        setErrMsg("Voting failed, please try again");
+
+        setVoteDifference((oldState) => oldState - value);
+        setVoteCount((oldState) => oldState - value);
+      });
   }
 
   return (
