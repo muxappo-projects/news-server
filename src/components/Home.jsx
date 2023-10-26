70811aa1a97bec9359e5b981670e145d5c348bdf
Max Field <maxfield98@outlook.com>
2023-10-26 19:27:37 +0100

    added simple existing user verification

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,18 @@
 import NavBar from "./NavBar";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { User } from "../contexts/User";
 
 export default function Home() {
+  const { currentUser } = useContext(User);
   return (
     <div>
       <NavBar />
-      <Link to="/login">
-        <button className="login-button">Login</button>
-      </Link>
+      {!currentUser && (
+        <Link to="/login">
+          <button className="login-button">Login</button>
+        </Link>
+      )}
     </div>
   );
 }
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,21 +1,39 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { User } from "../contexts/User";
+import * as api from "../utils/api";
 
 export default function LoginPage() {
-  const [userInput, setUserInput] = useState("");
   const { setCurrentUser } = useContext(User);
-
+  const [userInput, setUserInput] = useState("");
+  const [rawUserData, setRawUserData] = useState([]);
+  const [invalidUser, setInvalidUser] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    api.fetchUsers().then((users) => {
+      setRawUserData(users);
+    });
+  }, []);
 
   function handleSubmit(e) {
     e.preventDefault();
-    setCurrentUser(userInput);
-    navigate(-1);
+    const usernames = rawUserData.map((user) => user.username);
+
+    if (usernames.includes(userInput)) {
+      setCurrentUser(userInput);
+      navigate(-1);
+    }
+
+    setInvalidUser(true);
+    setTimeout(() => {
+      setInvalidUser(false);
+    }, 2000);
   }
 
   return (
     <form onSubmit={handleSubmit}>
+      {invalidUser && <p>Username doesn't exist!</p>}
       <label htmlFor="username-input">Username</label>
 
       <input
